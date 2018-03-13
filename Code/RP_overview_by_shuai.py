7cf640993006f9111038a11d55c8d76925811a3a
Shuai Huang <shuai9532@gmail.com>
2018-03-12 23:19:31 -0700

    fix indentation

--- Code/RP_overview_by_shuai.py
+++ Code/RP_overview_by_shuai.py
@@ -15,14 +15,13 @@ import collections as c
 
 #####open file#####
 def openfile(filename):
-	'''
-	open and read the file into the list
-
-	:param filename: name of the file
-	:type filename: str with the postfix .csv representing csv file
-	'''
-	assert isinstance(filename, str)
-	assert filename[-4:] == '.csv'
+    '''
+    open and read the file into the list
+    :param filename: name of the file
+    :type filename: str with the postfix .csv representing csv file
+    '''
+    assert isinstance(filename, str)
+    assert filename[-4:] == '.csv'
 
     content = []
     with open(filename, 'r') as csvfile:
@@ -46,18 +45,18 @@ for source in sources:
 
 
 def get_month_keys(keys):
-	'''
-	keys are the different months, but for the purpose of the project,
-	we only want the months with data. After viewing every months, we found
-	that last 70 months over the course of roughly 6 years are good amount 
-	of quality data
-
-	:param keys: list of name of months
-	:type keys: list	
-	'''
-	assert isinstance(keys, list)
-	assert len(keys) >= 70
-	for i in keys:                                                              
+    '''
+    keys are the different months, but for the purpose of the project,
+    we only want the months with data. After viewing every months, we found
+    that last 70 months over the course of roughly 6 years are good amount 
+    of quality data
+    
+    :param keys: list of name of months
+    :type keys: list	
+    '''
+    assert isinstance(keys, list)
+    assert len(keys) >= 70
+    for i in keys:                                                              
         assert isinstance(i, str) 
     return keys[-70:]
 
@@ -65,15 +64,15 @@ def get_month_keys(keys):
 def get_attr(keys):
     '''                                                                            
     keys are the different atributes like region, counties, but for the 
-	purpose of the project, we only want some of them, to name a few:
-	RegionName, CountyName, Metro.                                                                
-                                                                                   
+    purpose of the project, we only want some of them, to name a few:
+    RegionName, CountyName, Metro.                                                                
+    
     :param keys: list of name of attributes                                          
     :type  keys: list 
-	'''
-	assert isinstance(keys, list)
-	assert len(keys) >= 5
-	for i in keys:                                                              
+    '''
+    assert isinstance(keys, list)
+    assert len(keys) >= 5
+    for i in keys:                                                              
         assert isinstance(i, str) 
     return keys[:6]
 
@@ -84,24 +83,23 @@ keys= attr + months
 
 
 def form_list(keys, content):
-	'''
-	reform the dictioanry that incorporates the new set of keys
-	
-	:param keys: new set of keys
-	:type keys: list
-	
-	:param content: a list of lists that contain info for each palce
-	:type  content: a list of lists
-	'''
-	assert isinstance(keys, list)
-	assert len(keys) == 75
-	for i in keys:
-		assert isinstance(i, str)
-	assert isinstance(content, list)
-	assert len(content) > 0
-	for i in content:
-		assert isinstance(i, list)
-		assert len(i) > 0
+    '''
+    reform the dictioanry that incorporates the new set of keys	
+    :param keys: new set of keys
+    :type keys: list	
+    
+    :param content: a list of lists that contain info for each palce
+    :type  content: a list of lists
+    '''
+    assert isinstance(keys, list)
+    assert len(keys) == 75
+    for i in keys:
+	assert isinstance(i, str)
+    assert isinstance(content, list)
+    assert len(content) > 0
+    for i in content:
+	assert isinstance(i, list)
+	assert len(i) > 0
 
     ls = []
     for line in content:
@@ -114,24 +112,24 @@ def form_list(keys, content):
     return ls
 
 def preprocess_data(keys, datafiles):
-	'''
-	for each of four files, we preprocess the data by move the data into the
-	data structure and reform it in the preffered way using new keys
-	
-	:param keys: list of new keys
-	:type keys: list
-	
-	:param datafiles: dictionary of files with value of its content
-	:type datafiles: dictionary  
-	'''	
-	assert isinstance(keys, list)
-	assert len(keys) > 0
-	for i in keys:                                                              
-		assert isinstance(i, str) 
-	assert isinstance(datafiles, dict)
-	for i in datafiles:                                                              
+    '''
+    for each of four files, we preprocess the data by move the data into the
+    data structure and reform it in the preffered way using new keys
+    
+    :param keys: list of new keys
+    :type keys: list	
+    
+    :param datafiles: dictionary of files with value of its content
+    :type datafiles: dictionary  
+    '''	
+    assert isinstance(keys, list)
+    assert len(keys) > 0
+    for i in keys:                                                              
+	assert isinstance(i, str) 
+    assert isinstance(datafiles, dict)
+    for i in datafiles:                                                              
         assert isinstance(datafiles[i], list) 
-		assert len(datafiles[i]) > 0
+	assert len(datafiles[i]) > 0
 
     dataset = {}
     for i in datafiles:
@@ -142,23 +140,22 @@ def preprocess_data(keys, datafiles):
 dataset = preprocess_data(keys, datafiles)
 
 def astype_float(dataset, months):
-	'''
-	cleaning up ther data: if there is empty, filled it with previous data
-	else, convert the data to float for future computation
+    '''
+    cleaning up ther data: if there is empty, filled it with previous data\
+    else, convert the data to float for future computation
 	
-	:param dataset: the whole dataset loaded file after reformation
-	:type dataset: dictionary
-
-	:param months: a list of months
-	:type months: list 
-	'''	
-	assert len(dataset) == 4
-	assert isinstance(dataset, dict)
-	for i in dataset:
-		assert isinstance(i, list)
-		assert len(i) > 0
-	assert isinstance(months, list)
-	assert len(months) == 70
+    :param dataset: the whole dataset loaded file after reformation
+    :type dataset: dictionary
+    
+    :param months: a list of months
+    :type months: list 
+    '''	
+    assert isinstance(dataset, dict)
+    for i in dataset:
+	assert isinstance(i, list)
+	assert len(i) > 0
+    assert isinstance(months, list)
+    assert len(months) == 70
 	
     for i in dataset:
         ls = dataset[i]
@@ -174,24 +171,24 @@ dataset = astype_float(dataset, months)
 
 
 def get_data(category, dataset, s = sources):
-	'''
-	find the data inside the data collection by specifying the type of the data
+    '''
+    find the data inside the data collection by specifying the type of the data
 	
-	:param category: type of data we want to get
-	:type category: str
-	
-	:param dataset: a collection of 4 different dataset
-	:type dataset: dicionary
-	
-	:param s: the key that directs to the data content
-	:type s: list
-	'''
-	assert isinstance(s, list)
-	assert len(s) > 0	
-	assert isinstance(category, str)                                                  
+    :param category: type of data we want to get
+    :type category: str
+    
+    :param dataset: a collection of 4 different dataset
+    :type dataset: dicionary
+    
+    :param s: the key that directs to the data content
+    :type s: list
+    '''
+    assert isinstance(s, list)
+    assert len(s) > 0	
+    assert isinstance(category, str)                                                  
     assert len(category > 0
-	assert isinstance(dataset, dict)
-	assert len(dataset) > 0
+    assert isinstance(dataset, dict)
+    assert len(dataset) > 0
     for i in dataset:                                                           
         assert isinstance(i, list)                                              
         assert len(i) > 0 
@@ -207,28 +204,28 @@ def get_data(category, dataset, s = sources):
 
 ######################process teh data for graph#########################
 def calc_average(name, dataset, m = months, excludes = 'San Diego'):
-	'''
-	calculate the average that excludes certain area
-	
-	:param name: type of data, what data the user wants
-	:type name: str
-
-	:param months: a list of months
-	:type months: list
-
-	:param excludes: the arae that wants to be excluded in the calculation
-	:type excludes: str
-	'''
-	assert isinstance(name, str)
-	assert len(name) > 0 
+    '''
+    calculate the average that excludes certain area
+    
+    :param name: type of data, what data the user wants
+    :type name: str
+    
+    :param months: a list of months
+    :type months: list
+    
+    :param excludes: the arae that wants to be excluded in the calculation
+    :type excludes: str
+    '''
+    assert isinstance(name, str)
+    assert len(name) > 0 
     assert len(dataset) > 0                                                     
     for i in dataset:                                                           
         assert isinstance(i, list)                                              
         assert len(i) > 0    
-	assert isinstance(m, list)
-	assert len(m) > 0
-	assert isinstance(excludes, str)
-	assert len(excludes) > 0
+    assert isinstance(m, list)
+    assert len(m) > 0
+    assert isinstance(excludes, str)
+    assert len(excludes) > 0
 
     month_average = []
     for month in m:
@@ -243,32 +240,32 @@ sd =[92101,92037, 92109, 92122]
 
 def get_plotdata(name, county, dataset, selection_zip = sd):
     '''
-	pot data for average over months for default areas, san diego areas
+    pot data for average over months for default areas, san diego areas
+    
+    :param name: name of the data wnated 
+    :type name: str
 	
-	:param name: name of the data wnated 
-	:type name: str
+    :param county: county name
+    :type county: str
 	
-	:param county: county name
-	:type county: str
-	
-	:param dataset: a collection of data
-	:type dataset: a dictionary that has filename as key and content as value
-	
-	:param selection_zip: the areas that are desired
-	:type selection_zip: a list of strs
-	'''	
-	assert isinstance(name, str)
-	assert len(name) > 0
-	assert isinstance(county, str)
-	assert len(county) > 0
+    :param dataset: a collection of data
+    :type dataset: a dictionary that has filename as key and content as value
+
+    :param selection_zip: the areas that are desired
+    :type selection_zip: a list of strs
+    '''	
+    assert isinstance(name, str)
+    assert len(name) > 0
+    assert isinstance(county, str)
+    assert len(county) > 0
     assert len(dataset) > 0                                                     
     for i in dataset:                                                           
         assert isinstance(i, list)                                              
         assert len(i) > 0 
-	assert isinstance(selection_zip, list)
-	assert len(selection_zip) > 0
+    assert isinstance(selection_zip, list)
+    assert len(selection_zip) > 0
 
-	data = get_data(name, dataset)
+    data = get_data(name, dataset)
     target_areas = [d for d in data if d['City'] == county and int(d['RegionName']) in selection_zip]
     target_index = [data.index(d) for d in target_areas]
     target_months = [[d[month] for month in months] for d in target_areas]
@@ -279,31 +276,29 @@ sd_areas, sd_months, sd_indices = get_plotdata('zip_home', 'San Diego', dataset)
 sd_areas_rent, sd_months_rent, sd_indices_rent = get_plotdata('zip_rent', 'San Diego', dataset)
 
 def plot_month_price(name, target_areas, target_months, target_index, month_average, m = months):
-	'''
-	plot the data for average monthly change
+    '''
+    plot the data for average monthly change
 	
-	:param name: type of value -> home value or rent
-	:type name: str
-	
-	:param target_areas: list of desired areas with highlight
-		   target_months: list of months
-		   target_index:  list of indices of the areas in the dataset for emphasis
-	:type target_areas, target_months, target_index: list
-	
-	:param months_average: list of rent/home value over months
-	:type months_average: list
-	
-	:param m: list of months
-	:type m:list
-	'''
-	assert isinstance(name, str)
-	assert len(name) > 0
-	assert isinstance(target_areas, list)
-	assert isinstance(target_months, list) 
-	assert isinstance(target_index, list) 
-	assert isinstance(target_months, list) 
-	assert isinstance(m, list) 
-	assert len(m) > 0 && len(month_average) > 0
+    :param name: type of value -> home value or rent
+    :type name: str
+    
+    :param target_areas: list of desired areas with highlight
+    	   target_months: list of months
+           target_index:  list of indices of the areas in the dataset for emphasis
+    :type target_areas, target_months, target_index: list
+    :param months_average: list of rent/home value over months
+    :type months_average: list
+    :param m: list of month
+    :type m:list
+    '''
+    assert isinstance(name, str)
+    assert len(name) > 0
+    assert isinstance(target_areas, list)
+    assert isinstance(target_months, list) 
+    assert isinstance(target_index, list) 
+    assert isinstance(target_months, list) 
+    assert isinstance(m, list) 
+    assert len(m) > 0 && len(month_average) > 0
 
     plt.title('average %s with months in cities in USA vs. San Diego'%name)
     r = month_average
@@ -342,29 +337,28 @@ for i in counties:
 targets = ['San Diego', 'New York', 'Santa Clara', 'San Francisco']
 
 def plot_counties(category, new_data, c_list, m = months):
-	'''
-	plot the rent for each county in last mointh 2018-01 with some
-	highlights
+    '''
+    plot the rent for each county in last mointh 2018-01 with some highlights
+    
+    :param category: rent/home val
+    :type category: str
 	
-	:param category: rent/home val
-	:type category: str
+    :param new_data: list of dictionary that stores county name and rent
+    :type new_data: list of dictionary
 	
-	:param new_data: list of dictionary that stores county name and rent
-	:type new_data: list of dictionary
+    :param c_list: county list desired
+    :type c_list: list
 	
-	:param c_list: county list desired
-	:type c_list: list
-	
-	:param m : months
-	:type m: list
-	'''
-        assert isinstance(category, str)
-	assert len(category) > 0
-	assert isinstance(new_data, list)
-	assert len(new_data) > 0
-	assert isinstance(c_list, list)
-	assert len(c_list) > 0
-        assert isinstance(m, list)    
+    :param m : months
+    :type m: list
+    '''
+    assert isinstance(category, str)
+    assert len(category) > 0
+    assert isinstance(new_data, list)
+    assert len(new_data) > 0
+    assert isinstance(c_list, list)
+    assert len(c_list) > 0
+    assert isinstance(m, list)    
     assert len(m) > 0                                                      
 
     selected_month1 = m[-1]
